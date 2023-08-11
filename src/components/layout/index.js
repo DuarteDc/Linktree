9c4040e781c5eeb53d476b97a0034c9184cfba70
DuarteDc <al221910409@gmail.com>
2023-08-10 18:25:55 -0600

    add google tag manager

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,9 +1,13 @@
 import GoogleAnalitycs from './GoogleAnalitycs.astro';
 import Seo from './Seo.astro';
 import StructuredData from './StructuredData.astro';
+import GoogleTagManager from './GoogleTagManager.astro';
+import NoScriptGTM from './NoScriptGTM.astro';
 
 export {
     GoogleAnalitycs,
     Seo,
-    StructuredData
+    StructuredData,
+    GoogleTagManager,
+    NoScriptGTM
 }
