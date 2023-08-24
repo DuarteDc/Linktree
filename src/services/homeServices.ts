08c195543064568b6f218c9736a214ef30873f86
DuarteDc <al221910409@gmail.com>
2023-08-24 16:52:32 -0600

    resolve promise with promise all function

--- src/services/homeServices.ts
+++ src/services/homeServices.ts
@@ -1,23 +1,23 @@
 import { EntryCollection } from 'contentful';
 import { type LinksItemResponse, type UserResponse, ContentTypes, contentfulClient } from '../lib/contentful';
 
-const getPersonalLinksData = await contentfulClient.getEntries<LinksItemResponse>({
+const getPersonalLinksData = contentfulClient.getEntries<LinksItemResponse>({
     content_type: ContentTypes.PersonalLinks,
     order: ["sys.createdAt"],
 });
 
-const getProfesionalLinksData = await contentfulClient.getEntries<LinksItemResponse>({
+const getProfesionalLinksData = contentfulClient.getEntries<LinksItemResponse>({
     content_type: ContentTypes.ProfesionalLinks,
     order: ["sys.createdAt"],
 });
 
-const getUserData = await contentfulClient.getEntries<UserResponse>({
+const getUserData = contentfulClient.getEntries<UserResponse>({
     content_type: ContentTypes.User,
 });
 
 export default [
-    getPersonalLinksData as EntryCollection<LinksItemResponse, undefined, string>,
-    getProfesionalLinksData as EntryCollection<LinksItemResponse, undefined, string>,
-    getUserData as EntryCollection<UserResponse, undefined, string>,
+    getPersonalLinksData as Promise<EntryCollection<LinksItemResponse, undefined, string>>,
+    getProfesionalLinksData as Promise<EntryCollection<LinksItemResponse, undefined, string>>,
+    getUserData as Promise<EntryCollection<UserResponse, undefined, string>>,
 ]
 
